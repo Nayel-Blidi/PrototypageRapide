348dcc188d8c4b7285d540d0645a93ed98f82d9f
Nayel-Blidi <nayel.blidi@ipsa.fr>
2023-10-29 13:09:28 +0100

    bpsk_light class, for nn training

--- PSK.py
+++ PSK.py
@@ -340,6 +340,91 @@ class BPSK():
         
         return plt.show()
 
+class BPSK_light():
+    """
+    A lighter BPSK simulation class, dedicated to minimizing the computation time by
+    only simulating the sampled values.
+    """
+
+    def __init__(self, 
+                 message=None,
+                 word_length=1, words_number=12, 
+                 sigma=1,
+                 Fc=4, Fs=40, Rb=1,
+                 timeResolution=200,
+                 visualizations=False,
+                 ):
+        
+        self.message = message
+        self.word_length = round(word_length)
+        self.words_number = round(words_number)
+        self.sigma = sigma
+
+        # We take a bit ratio equal to the carrier frequency for simplicity. If we want to tweak any of these parameters,
+        # it can be acheived by oversampling the message (lower Rb, higher Fc) or cutting the signal (lower Fc, higher Rb)
+        # Lowering the bit ratio makes it more robust (longer pattern for one bit) = oversampling, 
+        # the same bit is repeted Fc/Rb times, and then the next bit is emitted by the carrier
+        self.Fc = Fc # Carrier frequency
+        self.Rb = Rb # Bit frequency (ratio)
+        self.Fs = Fs # Sampling frequency
+        self.Tc = 1/Fc
+        self.Ts = 1/Fs
+        
+        self.timeResolution = timeResolution # Simulation parameter, unreal. Higher returns more detailled plots and reduces performances.
+        self.visualizations = visualizations
+        self._message = self.message
+
+        if word_length != 1:
+            print("WARNING: BPSK modulation is intended for 1 bit long words.")
+
+
+    def sequenceGenerator(self):
+        """
+        Method \n
+        Generates a binary sequence according to the class parameters
+        """
+        self.message = np.round(np.random.rand(self.words_number, self.word_length)).astype(np.int32)
+        self.sequence = self.message
+
+        self.message = 2 * self.message - 1
+        self.bpsk = self.message
+
+        return self.sequence
+    
+    
+    def signalModulation(self):
+        """
+        Method \n
+        Modulates the keyed message into its temporal resolution version 
+        """
+        Fc = self.Fc
+        Rb = self.Rb
+        Fs = self.Fs
+        step = 1
+        T = round(Fs//Rb)
+        t = np.linspace(0, 1/Rb, T) # Time vector, timeResolution is a simulation parameter
+
+        temporal_message = np.zeros((self.words_number, self.word_length*T))
+        modulated_message = np.zeros((self.words_number, self.word_length*T))
+        for row_idx, row in enumerate(self.message):
+            temporal_message[row_idx, :] = row * np.cos(2 * np.pi * t * Rb)
+            modulated_message[row_idx, :] = row * np.cos(2 * np.pi * t * Fc)
+
+        self.temporal_message = temporal_message 
+        self.modulated_message = modulated_message
+        self.message = modulated_message
+        return self.message
+    
+    def gaussianNoise(self):
+        """
+        Method \n
+        Noises the modulated signal, as if it was emitted through an Additive White Noise Channel (AWGN)
+        """
+        self.message = self.message + np.random.rand(*self.message.shape) * self.sigma
+        self.noised = self.message
+
+        return self.message 
+    
 # %%
 import time
 """
@@ -357,14 +442,37 @@ bpsk_pipeline = [
     BPSK.grayDemapping,
 ]
 
-bpsk_class = BPSK(word_length=1, words_number=1e3, sigma=2)
-for step in tqdm(bpsk_pipeline, desc="BPSK Pipeline progression"):
-    message = step(bpsk_class)
-    time.sleep(0.1)
-np.savetxt("BPSK_output.txt", message, delimiter=",", fmt="%d")
+if __name__ == "__main__" and "bpsk" in sys.argv:
+    bpsk_class = BPSK(word_length=1, words_number=1e3, sigma=2)
+    for step in tqdm(bpsk_pipeline, desc="BPSK Pipeline progression"):
+        message = step(bpsk_class)
+        time.sleep(0.1)
 
-if __name__ == "__main__" and "plots" in sys.argv:
-    bpsk_class.stepVisualization(show=True)
+    if __name__ == "__main__" and "plots" in sys.argv:
+        bpsk_class.stepVisualization(show=True)
+
+
+bpsk_light_pipeline = [
+    BPSK_light.sequenceGenerator,
+    BPSK_light.signalModulation,
+    BPSK_light.gaussianNoise
+]
+
+if __name__ == "__main__" and ("bpsk_light" in sys.argv or "light" in sys.argv):
+    bpsk_light_class = BPSK_light(word_length=1, words_number=8, sigma=2,
+                                  Fs=40, Fc=4, Rb=1)
+    Y = bpsk_light_pipeline[0](bpsk_light_class)
+    bpsk_light_pipeline[1](bpsk_light_class)
+    X = bpsk_light_pipeline[2](bpsk_light_class)
+    np.savetxt("BPSK_output.txt", X, delimiter=",", fmt="%d")
+
+    if __name__ == "__main__" and "plots" in sys.argv:
+        plt.plot(X[0,:], label=f"{Y[0]}")
+        plt.plot(X[1,:], label=f"{Y[1]}")
+        plt.plot(X[2,:], label=f"{Y[2]}")
+        plt.plot(X[3,:], label=f"{Y[3]}")
+        plt.legend(loc="upper right")
+        plt.show()
 
 if __name__ == "__main__" and "cos" in sys.argv:
     t = np.linspace(0, np.pi, 100)
