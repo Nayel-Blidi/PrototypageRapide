7ae1140f28e1139db7641506ee35007511cbf5eb
Nayel-Blidi <nayel.blidi@ipsa.fr>
2023-10-29 11:36:19 +0100

    carrier modulation updated

--- PSK.py
+++ PSK.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from tqdm import tqdm
 import sys
+from scipy import signal
+from scipy.signal import hilbert
+
 """
 En BPSK, pour chaque unité de transmission (Tc=1/Fc carrier frequency), on transmet 1 valeur à 2 possibilités, 0 ou 1
 En QPSK, 1 valeur à 4 possibilités : 00, 01, 10, 11
@@ -22,14 +25,14 @@ class BPSK():
                  message=None,
                  word_length=1, words_number=2, 
                  sigma=1,
-                 Fc=2e3, Fs=10e3, Rb=2e3,
-                 timeResolution=100,
+                 Fc=4e2, Fs=20e2, Rb=1e2,
+                 timeResolution=10e2,
                  visualizations=False,
                  ):
         
         self.message = message
-        self.word_length = word_length
-        self.words_number = words_number
+        self.word_length = round(word_length)
+        self.words_number = round(words_number)
         self.sigma = sigma
 
         # We take a bit ratio equal to the carrier frequency for simplicity. If we want to tweak any of these parameters,
@@ -50,12 +53,22 @@ class BPSK():
             print("WARNING: BPSK modulation is intended for 1 bit long words.")
 
 
+    def _LowPassFilter(self, modulated_message, t):
+        filtre =  np.cos(2 * np.pi * self.Rb * t) #/ (2 * np.pi * t)
+        demod = modulated_message * 2 * np.cos(2*np.pi*self.Fc*t)
+        return np.convolve(filtre, demod)
+
+    def _ButterwothFilter(self, modulated_message):
+        b, a = signal.butter(N=2, Wn=0.1, btype='low')
+        return signal.filtfilt(b, a, modulated_message)
+    
+
     def sequenceGenerator(self):
         """
         Method \n
         Generates a binary sequence according to the class parameters
         """
-        self.message = np.round(np.random.rand(self.words_number, self.word_length))
+        self.message = np.round(np.random.rand(self.words_number, self.word_length)).astype(np.int32)
         self.sequence = self.message
         return self.message
     
@@ -69,6 +82,7 @@ class BPSK():
         self.mapping = self.message
         return self.message
     
+
     def BPSK(self):
         """
         Method \n
@@ -78,19 +92,14 @@ class BPSK():
         self.bpsk = self.message
         return self.message
     
+
     def signalOversampling(self):
         """
         Method \n
         Oversamples the signal to introduce bit redundancy
         """
         pass
-    
-    def timeOversampling(self):
-        """
-        Method \n
-        Oversamples the bit message (discrete) into a time vector
-        """
-        pass
+
 
     def signalModulation(self):
         """
@@ -99,37 +108,55 @@ class BPSK():
         """
         Fc = self.Fc
         Rb = self.Rb
-        T = self.timeResolution
-        t = np.linspace(0, T, T) # Time vector, timeResolution is a simulation parameter
+        T = round(self.timeResolution)
+        t = np.linspace(0, 1/Rb, T) # Time vector, timeResolution is a simulation parameter
 
         temporal_message = np.zeros((self.words_number, self.word_length*T))
+        modulated_message = np.zeros((self.words_number, self.word_length*T))
         for row_idx, row in enumerate(self.message):
             for col_idx, col in enumerate(row):
-                temporal_message[row_idx, col_idx*T:(col_idx+1)*T] = col * np.cos(2 * np.pi * (Fc/Rb) * t)   
+                temporal_message[row_idx, col_idx*T:(col_idx+1)*T] = col * np.cos(2 * np.pi * t * Rb)
+                # modulated_message[row_idx, col_idx*T:(col_idx+1)*T] = np.convolve(col *np.cos(2 * np.pi * t * Rb) , np.cos(2 * np.pi * Fc * t))[0:T]
+                modulated_message[row_idx, col_idx*T:(col_idx+1)*T] = col *np.cos(2 * np.pi * t * Fc)
 
-        self.message = temporal_message   
-        self.modulation = self.message
+        self.temporal_message = temporal_message 
+        self.modulated_message = modulated_message
+        self.message = modulated_message
         return self.message
     
+
     def gaussianNoise(self):
         """
         Method \n
         Noises the modulated signal, as if it was emitted through an Additive White Noise Channel (AWGN)
         """
-        self.message = self.message + np.random.rand(*self.message.shape) * self.sigma
-        self.noised = self.message
+        message = self.message + np.random.rand(*self.message.shape) * self.sigma
+        self.noised = message
+
+        # T = round(self.timeResolution)
+        # t = np.linspace(0, 1/self.Rb, T) # Time vector, timeResolution is a simulation parameter
+        # noisy_message = np.zeros(message.shape)
+        # for row_idx, row in enumerate(message):
+        #     # self.message[row_idx, :] = self._ButterwothFilter(modulated_message=row)
+        #     # self.message[row_idx, :] = np.angle((hilbert(row)))
+        #     noisy_message[row_idx, :] = self._LowPassFilter(modulated_message=row, t=t)[0:T]
+
+        # self.noised_demodulated = noisy_message
+        # self.message = noisy_message
         return self.message
     
+
     def signalSampling(self):
         """
         Method \n
         Samples the temporal signal, as if received by an oscillator
         """
-        T = self.timeResolution
+        T = round(self.timeResolution)
         Fs = self.Fs
         Fc = self.Fc
-        # A value is retreived every step, which results in Fs values every second
-        step = round(T/(Fs/Fc))
+        Rb = self.Rb
+        # A value is retreived every step, which results in Fs values every second (if possible, otherwise T values)
+        step = max(round(T/(Fs/Rb)), 1)
 
         temporal_message = np.zeros((self.words_number, self.word_length*T))
         for row_idx, row in enumerate(self.message):
@@ -139,10 +166,11 @@ class BPSK():
         self.sampled = temporal_message
         return self.message
     
+
     def sequenceDecider(self):
         """
         Method \n
-        Decides the value of every 
+        Decides the bit value of every received symbol
         """
         T = self.timeResolution
         Fs = self.Fs
@@ -184,6 +212,7 @@ class BPSK():
         self.message[self.message == -1] = 0
         return self.message
 
+
     def grayDemapping(self):
         """
         Method \n
@@ -193,7 +222,8 @@ class BPSK():
         self.demapped = self.message
         return self.message
 
-    def stepVisualization(self, max_itt=2, show=True):
+
+    def stepVisualization(self, max_itt=2, show=False):
         """ 
         Function \n
         Plots the different steps of the BPSK process
@@ -206,29 +236,33 @@ class BPSK():
             max_itt = 4
 
         # %% Bit sequence plot
-        plt.figure()
+        plt.subplots(2, 1)
         plt.suptitle("Message sequence")
-        plt.grid(True)
         try:
             message = self._message
             N = len(message)
-            plt.bar(np.linspace(0, N, N), message, label="Bits sequence")
+            plt.subplot(2, 1, 1)
+            plt.bar(np.linspace(0, N-1, N), message, label="Bits sequence")
+            plt.title("Bits sequence")
         except:
             None
         try:
             message = self.sequence.flatten()
             N = len(message)
-            plt.bar(np.linspace(0, N, N), message, label="Bits sequence")
+            plt.subplot(2, 1, 1)
+            plt.bar(np.linspace(0, N-1, N), message, label="Bits sequence")
+            plt.title("Bits sequence")
         except:
             error_message = "No message nor sequence to plot. initialization inputs or the sequenceGenerator() step"
             print(error_message)
 
         # %% BPSK sequence
         try:
-            plt.figure()
+            plt.subplot(2, 1, 2)
             message = self.bpsk.flatten()
             N = len(message)
-            plt.bar(np.linspace(0, N, N), message, label=f"message={(message+1)//2}")
+            plt.bar(np.linspace(0, N-1, N), message, label=f"message={(message+1)//2}")
+            plt.title("BPSK encoded sequence")
             if plotLegend:
                 plt.legend(loc="upper right")
         except:
@@ -237,14 +271,16 @@ class BPSK():
             
         # %% Time resolution signal
         try:
-            itt=0
-            plt.subplots(max_itt, 1)
-            while itt<max_itt:
-                plt.subplot(max_itt, 1, itt+1)
-                plt.plot(self.modulation[itt, :], label=f"word={self.sequence[itt,:]}")
-                if plotLegend:
-                    plt.legend(loc="upper right")
-                itt+=1
+            plt.subplots(4, 1)
+            plt.subplot(4, 1, 1)
+            plt.plot(self.temporal_message[0, :], label=f"bit={self.sequence.flatten()[0]}")
+            plt.title("temporal_message")
+            plt.subplot(4, 1, 2)
+            plt.plot(self.modulated_message[0, :], label=f"bit={self.sequence.flatten()[0]}")
+            plt.title("modulated_message")
+
+            if plotLegend:
+                plt.legend(loc="upper right")
             plt.suptitle(f"Temporal resolution modulated signal")
         except:
             error_message = "No modulated signal to plot. Verify the signalModulation() step."
@@ -252,30 +288,22 @@ class BPSK():
         
         # %% Noisy signal
         try:
-            itt=0
-            plt.subplots(max_itt, 1)
-            while itt<max_itt:
-                plt.subplot(max_itt, 1, itt+1)
-                plt.plot(self.noised[itt, :], label=f"word={self.sequence[itt,:]}")
-                if plotLegend:
-                    plt.legend(loc="upper right")
-                itt+=1
-            plt.suptitle(f"Noisy signal")
+            plt.subplot(4, 1, 3)
+            plt.plot(self.noised[0, :], label=f"bit={self.sequence.flatten()[0]}")
+            plt.title("noised_message")
+            if plotLegend:
+                plt.legend(loc="upper right")
         except:
             error_message = "No noised signal to plot. Verify the gaussianNoise() step."
             print(error_message)
 
         # %% Time resolution signal
         try:
-            itt=0
-            plt.subplots(max_itt, 1)
-            while itt<max_itt:
-                plt.subplot(max_itt, 1, itt+1)
-                plt.plot(self.sampled[itt, :], label=f"word={self.sequence[itt,:]}")
-                if plotLegend:
+            plt.subplot(4, 1, 4)
+            plt.plot(self.sampled[0, :], label=f"bit={self.sequence.flatten()[0]}")
+            plt.title(f"Sampled signal")
+            if plotLegend:
                     plt.legend(loc="upper right")
-                itt+=1
-            plt.suptitle(f"Sampled signal")
         except:
             error_message = "No modulated signal to plot. Verify the signalSampling() step."
             print(error_message)
@@ -287,12 +315,12 @@ class BPSK():
             message_decoded = self.decided.flatten()
             N = len(message_decoded)
             plt.subplot(2, 1, 1)
-            plt.bar(np.linspace(0, N, N), message_encoded, label=f"message encoded={message_encoded}")
+            plt.bar(np.linspace(0, N, N), message_encoded, label=f"message encoded={message_encoded[:]}")
             plt.title("Encoded message")
             if plotLegend:
                 plt.legend(loc="upper right")
             plt.subplot(2, 1, 2)
-            plt.bar(np.linspace(0, N, N), message_decoded, label=f"message decoded={message_decoded}")
+            plt.bar(np.linspace(0, N, N), message_decoded, label=f"message decoded={message_decoded[:]}")
             plt.title("Decoded message")
             if plotLegend:
                 plt.legend(loc="upper right")
@@ -335,7 +363,7 @@ bpsk_pipeline = [
     BPSK.grayDemapping,
 ]
 
-bpsk_class = BPSK(word_length=2, words_number=4)
+bpsk_class = BPSK(word_length=1, words_number=12)
 for step in tqdm(bpsk_pipeline, desc="BPSK Pipeline progression"):
     message = step(bpsk_class)
     time.sleep(0.1)
